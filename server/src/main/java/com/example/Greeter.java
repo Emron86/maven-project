c71bb6ca2c23c0f613e89f6454c94152c310d138
Våhlström <emil.vahlstrom@cgi.com>
2018-07-24 11:10:35 +0200

    add package-info.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
+  /**
+   * @param someone the name of the person
+   * @return greeting string
+   */
+  public String greet(final String someone) {
+    final return String.format("Hello, %s!", someone);
   }
 }
